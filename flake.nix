6a60f6714e463f93db8e0ffa717ba1d06decf58e
Max Generowicz <mdcg-github@my-post-office.net>
2026-04-18 13:58:17 +0000

    Initial flake environment

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    ratpac-nix.url = "github:mdcge/ratpac-nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ratpac-nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        ratpac = ratpac-nix.packages.${system}.ratpac-two;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [ ratpac pkgs.cmake pkgs.pkg-config ];
+          shellHook = ''
+            export CMAKE_PREFIX_PATH=${ratpac}:$CMAKE_PREFIX_PATH
+          '';
+        };
+      });
+}
